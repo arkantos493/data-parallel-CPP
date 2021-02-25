f1fd44eecb7f732148848abd77971c59390e72dd
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-02-25 10:53:46 +0100

    added defines and a namespace alias due to API issues with the newest oneTBB version (https://community.intel.com/t5/Intel-oneAPI-Threading-Building/tbb-task-has-not-been-declared/m-p/1255723?profile.language=de&countrylabel=Mexico)

--- samples/Ch18_using_libs/fig_18_11_std_fill.cpp
+++ samples/Ch18_using_libs/fig_18_11_std_fill.cpp
@@ -2,11 +2,21 @@
 
 // SPDX-License-Identifier: MIT
 
+// https://community.intel.com/t5/Intel-oneAPI-Threading-Building/tbb-task-has-not-been-declared/m-p/1255723?profile.language=de&countrylabel=Mexico
+#if defined(__GNUG__)
+#define PSTL_USE_PARALLEL_POLICIES 0
+#define _GLIBCXX_USE_TBB_PAR_BACKEND 0
+#endif
+
 #include <CL/sycl.hpp>
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/iterator>
+
 using namespace sycl;
+#if defined(__GNUG__)
+namespace dpstd = dpl;
+#endif
 
 int main() {
   queue Q{};
--- samples/Ch18_using_libs/fig_18_13_binary_search.cpp
+++ samples/Ch18_using_libs/fig_18_13_binary_search.cpp
@@ -2,13 +2,23 @@
 
 // SPDX-License-Identifier: MIT
 
+// https://community.intel.com/t5/Intel-oneAPI-Threading-Building/tbb-task-has-not-been-declared/m-p/1255723?profile.language=de&countrylabel=Mexico
+#if defined(__GNUG__)
+#define PSTL_USE_PARALLEL_POLICIES 0
+#define _GLIBCXX_USE_TBB_PAR_BACKEND 0
+#endif
+
 #include <CL/sycl.hpp>
 #include <cstdint>
 #include <iostream>
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/iterator>
+
 using namespace sycl;
+#if defined(__GNUG__)
+namespace dpstd = dpl;
+#endif
 
 int main() {
   buffer<std::uint64_t> kB{range<1>(10)};
